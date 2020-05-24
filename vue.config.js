2b6ea0e4d30244f3b70c028517728d5ece81190b
Denis Sales da Silva <denisales@hotmail.com>
2020-05-23 21:27:01 -0300

    ajustes no vue config, cor de fundo do pwa e cor da barra de endereços

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
+  productionSourceMap: false,
   transpileDependencies: [
     'vuetify',
   ],
+  pwa: {
+    name: 'Bexs',
+    themeColor: '#de4b4b',
+    msTileColor: '#de4b4b',
+    // configure the manifest options
+    manifestOptions: {
+      display: 'standalone',
+      background_color: '#de4b4b',
+      orientation: 'portrait',
+    },
+  },
 };
