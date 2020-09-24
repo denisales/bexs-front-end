7d38482349034caef0b28a7879b061e821cd94aa
Denis Sales <denisales@hotmail.com>
2020-09-24 10:07:06 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -12,6 +12,13 @@ module.exports = {
       display: 'standalone',
       background_color: '#de4b4b',
       orientation: 'portrait',
+      related_applications: [
+        {
+          platform: 'play',
+          url: 'https://play.google.com/store/apps/details?id=app.netlify.gracious_mayer_2d6411.twa',
+          id: 'app.netlify.gracious_mayer_2d6411.twa',
+        },
+      ],
     },
     workboxPluginMode: 'GenerateSW',
     workboxOptions: {
